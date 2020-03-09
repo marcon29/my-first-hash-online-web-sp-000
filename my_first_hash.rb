d4243ad6b41175672149c6869b0084814e481dca
Mark Stabler <mark@markstabler.com>
2020-03-09 17:04:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,10 +16,9 @@ def retrieval
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3
-                 }
+  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
