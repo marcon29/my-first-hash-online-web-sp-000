081ee4855756ba57ea6d6ab1780665465d31b5ab
Mark Stabler <mark@markstabler.com>
2020-03-09 16:59:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to
-  # a hash with key/value pairs of your choice.
-  # my_hash = {"first_name" => "Mark", "last_name" => "Stabler", "age" => 42}
+  my_hash = {"first_name" => "Mark", "last_name" => "Stabler", "age" => 42}
 end
 
 
